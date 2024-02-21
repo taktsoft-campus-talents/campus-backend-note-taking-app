51b8249351ad8027b7aab8caa6b052defdb2c682
Ernst Stolz <eric@spiritsdontfly.com>
2024-02-21 11:49:16 +0100

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const postgres = require("@vercel/postgres");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.get("/", async (request, response) => {
   createNotes();
