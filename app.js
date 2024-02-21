6ad6b81a27be7e4090f3ebc6850ca4a2ff11b64b
Ernst Stolz <eric@spiritsdontfly.com>
2024-02-21 12:40:29 +0100

    add post and individual get route

--- app.js
+++ app.js
@@ -4,12 +4,38 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (request, response) => {
   createNotes();
   //table was created => load data
   const { rows } = await postgres.sql`SELECT * FROM notes`;
   return response.json(rows);
+});
+
+app.get("/:id", async (request, response) => {
+  createNotes();
+  //table was created => load data
+  const { id } = request.params;
+  const { rows } = await postgres.sql`SELECT * FROM notes WHERE id = ${id}`;
+
+  if (!rows.length) {
+    return response.json({ error: "Note not found." });
+  }
+
+  return response.json(rows[0]);
+});
+
+app.post("/", async (request, response) => {
+  createNotes();
+  const { content } = JSON.parse(request.body);
+  if (content) {
+    await postgres.sql`INSERT INTO notes (content) VALUES (${content})`;
+    response.json({ message: "Successfully created note." });
+  } else {
+    response.json({ error: "Note NOT created. Content is missing." });
+  }
 });
 
 // default catch-all handler
